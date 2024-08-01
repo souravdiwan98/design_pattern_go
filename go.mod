ee34dfd1022b7587f58970f7d03acb47d17a77cd
Sourav Diwan <SODIWA-BLRM22@192.168.1.4>
2024-08-01 23:44:14 +0530

    Initial Project Files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/souradiwan98/design_pattern_go
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	composite "github.com/souradiwan98/design-patterns-go/composite-pattern"
+)
+
+func main() {
+	composite.Composite()
+}
